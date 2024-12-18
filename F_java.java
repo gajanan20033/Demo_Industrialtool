6ffbf2fdee68e5de96d6e0b291e50aa83e9a1bd5
gajanan20033 <gajananpatil9589@gmail.com>
2024-12-18 17:11:53 +0530

    adding some msg

--- F_java.java
+++ F_java.java
@@ -2,6 +2,8 @@ public class helloword {
 	public static void main(String arg[])
 {
 	System.out.println("HelloWord");
+	System.out.println("date:18/12/2024");
+	System.out.println("Goodbay 2024");
 }
 
 }
